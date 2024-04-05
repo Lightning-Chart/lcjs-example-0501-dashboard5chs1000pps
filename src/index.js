94371b69482adcc8f75b4b1dc95a8b004e0f1dc7
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2024-04-05 15:11:11 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -18,7 +18,9 @@ const channelCount = channels.length
 
 // Create Dashboard.
 // NOTE: Using `Dashboard` is no longer recommended for new applications. Find latest recommendations here: https://lightningchart.com/js-charts/docs/basic-topics/grouping-charts/
-const grid = lightningChart().Dashboard({
+const grid = lightningChart({
+            resourcesBaseUrl: new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pathname + 'resources/',
+        }).Dashboard({
     theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined,
     numberOfRows: channelCount,
     numberOfColumns: 1,
